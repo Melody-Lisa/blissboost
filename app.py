9fd2064293f98959d0148967cd329b6190eb883a
Melody Shutt <melody.lisa1990@gmail.com>
2024-03-07 14:15:27 +0000

    Create login functionality with extra styling

--- app.py
+++ app.py
@@ -40,11 +40,6 @@ def register():
             flash("Passwords do not match")
             return redirect(url_for("register"))
 
-        if request.form.get("username") or request.form.get(
-            "password") != request.form.get("pattern"):
-            flash("Usernames and passwords must only contain letters and/or numbers.")
-            return redirect(url_for("register"))
-
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password")),
@@ -55,6 +50,32 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration successful!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome back, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Invalid username or password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
