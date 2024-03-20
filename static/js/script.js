c339135965125f06d60480f05e04d96fee0044fb
Melody Shutt <melody.lisa1990@gmail.com>
2024-03-20 11:30:41 +0000

    Style side nav

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-      direction: 'left'
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'left'
   });
+});
 
-  function visibility(event) {
-    let element = event.target.parentNode.parentNode;
-    let input = element.querySelector("input");
-    if (input.type === 'password') {
-        input.type = 'text';
-        event.target.classList.remove('fa-eye-slash');
-        event.target.classList.add('fa-eye');
-    } else {
-        input.type = 'password';
-        event.target.classList.remove('fa-eye');
-        event.target.classList.add('fa-eye-slash');
-    }
+function visibility(event) {
+  let element = event.target.parentNode.parentNode;
+  let input = element.querySelector("input");
+  if (input.type === 'password') {
+    input.type = 'text';
+    event.target.classList.remove('fa-eye-slash');
+    event.target.classList.add('fa-eye');
+  } else {
+    input.type = 'password';
+    event.target.classList.remove('fa-eye');
+    event.target.classList.add('fa-eye-slash');
   }
+}
+
+function closeNav() {
+  var elem = document.querySelector('.sidenav');
+  var instance = M.Sidenav.getInstance(elem);
+  instance.close();
+}
