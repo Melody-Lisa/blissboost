67fa7a2836e9bd282c103999469bac9e7799bc8f
Melody Shutt <melody.lisa1990@gmail.com>
2024-03-22 18:01:53 +0000

    Remove white space and shorten lines

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 '''
-Code Institute non-relational database management systems module used to set up the app
+Code Institute non-relational database management systems
+module used to set up the app
 and ensure CRUD functionality.
 '''
 
 import os
 from flask import (
     Flask, flash, render_template,
-     redirect, request, session, url_for, send_from_directory, abort)
+    redirect, request, session, url_for, send_from_directory, abort)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import (
@@ -18,17 +19,20 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+
 # Define the custom 404 error handler learnt from stackoverflow
 @app.errorhandler(404)
 def page_not_found(error):
     # Render the 404.html template with any necessary context
     return render_template('404.html'), 404
 
+
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
+
 
 def is_valid_image_url(url):
     # List of common image file extensions
@@ -38,7 +42,7 @@ def is_valid_image_url(url):
     for extension in image_extensions:
         if url.lower().endswith(extension):
             return True
-    
+
     return False
 
 
@@ -61,9 +65,10 @@ def register():
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-        
+
         # check if passwords match
-        if request.form.get("password") != request.form.get("confirm_password"):
+        if request.form.get("password") != request.form.get(
+                "confirm_password"):
             flash("Passwords do not match")
             return redirect(url_for("register"))
 
@@ -91,11 +96,11 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome back, {}".format(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome back, {}".format(
                         request.form.get("username")))
-                    return redirect(url_for(
+                return redirect(url_for(
                         "profile", user=session["user"]))
             else:
                 # invalid password match
@@ -112,7 +117,7 @@ def login():
 
 @app.route("/logout")
 def logout():
-    #remove user from session cookies
+    # remove user from session cookies
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
@@ -127,7 +132,7 @@ def profile(user):
     if user_data:
         # Retrieve the liked post ObjectIds
         liked_post_ids = user_data.get("post_likes", [])
-        
+
         # Fetch the details of the liked posts
         liked_posts = []
         for post_id in liked_post_ids:
@@ -135,7 +140,8 @@ def profile(user):
             if post_details:
                 liked_posts.append(post_details)
 
-        return render_template("profile.html", user=user_data, liked_posts=liked_posts)
+        return render_template("profile.html",
+                               user=user_data, liked_posts=liked_posts)
     else:
         flash("User not found")
         return redirect(url_for("home"))
@@ -150,11 +156,14 @@ def upload_pics():
         # Check if the provided URL is not empty and is a valid image URL
         if profile_pic_url != "" and is_valid_image_url(profile_pic_url):
             # Save the URL to the user's profile
-            mongo.db.users.update_one({'username': user}, {"$set": {'profile_pic_url': profile_pic_url}})
+            mongo.db.users.update_one(
+                {'username': user},
+                {"$set": {'profile_pic_url': profile_pic_url}}
+                )
             return redirect(url_for("profile", user=user))
         else:
             flash("Please enter a valid image URL")
-        
+
         return redirect(url_for("profile", user=user))
     else:
         flash("You can't edit this page, please log in.")
@@ -165,7 +174,7 @@ def upload_pics():
 def edit_profile(user):
     # grab session user's username from the db
     logged_in_username = session.get("user")
-    
+
     if logged_in_username:
         if logged_in_username == user:
             if request.method == "POST":
@@ -177,7 +186,8 @@ def edit_profile(user):
                 }
 
                 # Perform the update operation
-                mongo.db.users.update_one({"username": user}, {"$set": edit_data})
+                mongo.db.users.update_one(
+                    {"username": user}, {"$set": edit_data})
 
                 flash("User details updated successfully")
                 return redirect(url_for("profile", user=user))
@@ -220,7 +230,8 @@ def search():
         query = request.form.get("query")
         posts = list(mongo.db.posts.find({"$text": {"$search": query}}))
     else:
-        # Handle form submission via GET request (e.g., when clicking on pagination links)
+        # Handle form submission via GET request
+        # (e.g., when clicking on pagination links)
         query = request.args.get("query")
         posts = list(mongo.db.posts.find({"$text": {"$search": query}}))
 
@@ -257,10 +268,11 @@ def edit_post(post_id):
     if logged_in_username:
         # Retrieve the post from the database
         post = mongo.db.posts.find_one({"_id": ObjectId(post_id)})
-        
+
         if post:
             # Check if the logged-in user is the author of the post or an admin
-            if logged_in_username == post.get("created_by") or logged_in_username.lower() == "admin":
+            if logged_in_username == post.get(
+                    "created_by") or logged_in_username.lower() == "admin":
                 if request.method == "POST":
                     theme_data = request.form.get('theme_data').split('|')
                     theme_name = theme_data[0]
@@ -270,21 +282,25 @@ def edit_post(post_id):
                             'theme_name': theme_name,
                             'theme_image': theme_image,
                             'post_title': request.form.get('post_title'),
-                            'post_description': request.form.get('post_description'),
+                            'post_description': request.form.get(
+                                'post_description'),
                         }
                     }
 
-                    # Exclude 'created_by' and 'post_date' fields from the update operation
+                    # Exclude 'created_by' and 'post_date'
+                    # fields from the update operation
                     if logged_in_username != post.get("created_by"):
-                            submit['$set']['created_by'] = post.get("created_by")
-                            submit['$set']['post_date'] = post.get("post_date")
+                        submit['$set']['created_by'] = post.get("created_by")
+                        submit['$set']['post_date'] = post.get("post_date")
 
-                    mongo.db.posts.update_one({"_id": ObjectId(post_id)}, submit)
+                    mongo.db.posts.update_one({"_id": ObjectId(post_id)},
+                                              submit)
                     flash("Post Successfully Updated")
                     return redirect(url_for("get_posts"))
 
                 themes = mongo.db.themes.find().sort("theme_name", 1)
-                return render_template("edit_post.html", post=post, themes=themes)
+                return render_template(
+                    "edit_post.html", post=post, themes=themes)
             else:
                 # User is not authorized to edit this post
                 flash("You are not authorized to edit this post.")
@@ -320,17 +336,19 @@ def like_post(_id):
             "_id": ObjectId(_id)}, {"$inc": {"post_likes": 1}})
 
     # Get updated likes count for the post
-    post_likes_count = mongo.db.posts.find_one({"_id": ObjectId(_id)})["post_likes"]
+    post_likes_count = mongo.db.posts.find_one(
+        {"_id": ObjectId(_id)})["post_likes"]
 
     # If likes count is 0, remove the post_likes field from the document
     if post_likes_count == 0:
-        mongo.db.posts.update_one({"_id": ObjectId(_id)}, {"$unset": {"post_likes": ""}})
+        mongo.db.posts.update_one(
+                {"_id": ObjectId(_id)}, {"$unset": {"post_likes": ""}})
 
     # Update session['liked'] to reflect the latest liked posts
     liked = mongo.db.users.find_one({
         "username": session["user"]}).get("post_likes", [])
     session["liked"] = liked
-    
+
     return redirect(request.referrer)
 
 
@@ -388,12 +406,13 @@ def edit_theme(theme_id):
                 "theme_name": request.form.get("theme_name"),
                 "theme_image": request.form.get("theme_image")
             }
-            mongo.db.themes.update_one({"_id": ObjectId(theme_id)}, {"$set": submit})
+            mongo.db.themes.update_one(
+                {"_id": ObjectId(theme_id)}, {"$set": submit})
             flash("Theme Successfully Updated")
             return redirect(url_for("get_themes"))
 
         theme = mongo.db.themes.find_one({"_id": ObjectId(theme_id)})
-        
+
         if theme:
             themes = mongo.db.themes.find()
             return render_template("edit_theme.html", theme=theme)
@@ -420,5 +439,5 @@ def delete_theme(theme_id):
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
