85a5f620574e00a0afcc80f894f55ed521b5adb1
Melody Shutt <melody.lisa1990@gmail.com>
2024-03-25 23:06:38 +0000

    Debug False

--- app.py
+++ app.py
@@ -471,4 +471,4 @@ def delete_theme(theme_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
